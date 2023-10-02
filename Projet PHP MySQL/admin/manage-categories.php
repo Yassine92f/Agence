bdf9faf3586f134b7d6f860992e58bffaca3a931
Yassine92f <yassinesw92@gmail.com>
2023-10-02 09:50:22 +0200

    Premier commit

---
+++ Projet PHP MySQL/admin/manage-categories.php
@@ -0,0 +1,9 @@
+<?php include('partials/menu.php'); ?>
+
+<div class="main-content">
+    <div class="wrapper">
+        <h1 class="text-center">Gestion des plats</h1>
+    </div>
+</div>
+
+<?php include('partials/footer.php'); ?>
---
+++ Projet PHP MySQL/traitement.php
@@ -0,0 +1,33 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nom = $_POST["nom"];
+    $email = $_POST["email"];
+    $adresse = $_POST["adresse"];
+    $tel = $_POST["tel"];
+    $message = $_POST["message"];
+
+    // Maintenant, vous avez les données du formulaire et vous pouvez les insérer dans votre base de données.
+    // Assurez-vous de prendre des mesures de sécurité appropriées, comme la validation et l'échappement des données, avant de les insérer dans la base de données.
+    
+    // Exemple d'insertion dans une base de données (vous devrez adapter ceci à votre propre configuration) :
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "agence";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $sql = "INSERT INTO contact (nom, email, adresse, tel, message) VALUES ('$nom', '$email', '$adresse', '$tel', '$message')";
+        
+        $conn->exec($sql);
+        
+        echo "Demande soumise avec succès !";
+    } catch (PDOException $e) {
+        echo "Erreur : " . $e->getMessage();
+    }
+
+    $conn = null;
+}
+?>
